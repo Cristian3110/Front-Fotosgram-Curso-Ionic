c149ddcd30a809486ee75b602e1cb12297e22690
cristian3110 <cristiancarrillo311087@gmail.com>
2020-08-28 03:12:27 -0400

    creando proyecto fotosgram, carpeta pages, y carpeta de servicios y hacer llamados al mismo

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // importando el servicio
+import { environment } from '../../environments/environment'; // importar del normal no el de producción
+
+
+
+const URL = environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  paginasPosts = 0; // inicio de las paginas
+
+  constructor(private http: HttpClient) { }
+
+
+
+  getPosts(){
+
+    this.paginasPosts ++; // llamado a las paginas
+
+    return this.http.get(`${URL}/posts/?pagina=1`);
+  }
+}
